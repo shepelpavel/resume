bffaaefed590073abeac1d01d2461642e42fa500
Shepel Pavel <pavel@shepel.net>
2021-04-25 14:21:09 +0300

    feat: init vue

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>This is an HelloWorld page</h1>
+    <HelloWorld msg="Welcome" />
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
