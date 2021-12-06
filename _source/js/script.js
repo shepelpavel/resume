e38d722adc433df78bdda5a34041072e78058f7c
Shepel Pavel <pavel@shepel.net>
2021-12-06 09:18:35 +0300

    feat: add libs

--- _source/js/script.js
+++ _source/js/script.js
@@ -2,30 +2,59 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function backgroundCanvasInit(_pixel, _width, _height) {
+function backgroundCanvasInit(_width, _height) {
     var canvas = document.getElementById('background_canvas');
     var context = canvas.getContext('2d');
 
     canvas.width = _width;
     canvas.height = _height;
 
+    var _pixel_length = Math.ceil(_width / 5);
+    var _PIXELS = [];
+    var _SPEED = 1;
+    var _step = Math.floor(_pixel_length / 50);
+    
+    for (var i = 0; i < _pixel_length; i++) {
+        var _modul = Math.sin(i / _step) * Math.sin(i / 10);
+        _PIXELS.push({
+            posX: _width / _pixel_length * i,
+            baseY: _height / 2,
+            posY: _height / 2,
+            modul: _modul,
+            width: 3,
+            height: 3,
+            color: '#cccccc'
+        })
+    }
+
     setInterval(function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
-        if (
-            _pixel.posY + _pixel.speed > _pixel.maxposY ||
-            _pixel.posY + _pixel.speed < _pixel.minposY
-        ) {
-            _pixel.speed = _pixel.speed * -1;
-        }
-        _pixel.posY += _pixel.speed;
+        for (_pixel of _PIXELS) {
+            _pixel.posY += _pixel.modul * _SPEED;
 
-        context.beginPath();
-        context.fillStyle = _pixel.color;
-        context.fillRect(_pixel.posX, _pixel.posY, _pixel.width, _pixel.height);
-        context.closePath();
-        context.fill();
+            var _shift = (_pixel.baseY - _pixel.posY) / 10
+            _pixel.posY += _shift;
+
+            context.beginPath();
+            context.fillStyle = _pixel.color;
+            context.fillRect(_pixel.posX, _pixel.posY, _pixel.width, _pixel.height);
+            context.closePath();
+            context.fill();
+        }
+
     }, 30);
+
+    var _PREVEVENT, _CURREVENT;
+    document.addEventListener("mousemove", function (event) {
+        _CURREVENT = event;
+        if (_PREVEVENT && _CURREVENT) {
+            var movementX = Math.abs(_CURREVENT.screenX - _PREVEVENT.screenX);
+            var movementY = Math.abs(_CURREVENT.screenY - _PREVEVENT.screenY);
+            _SPEED = Math.sqrt(movementX * movementX + movementY * movementY);
+        }
+        _PREVEVENT = _CURREVENT;
+    });
 }
 
 function backgroundGridInit(calc_v, calc_h) {
@@ -35,7 +64,6 @@ function backgroundGridInit(calc_v, calc_h) {
     var _hstatr_pos = (calc_h - (_hitems * 400)) / 2;
     var _vstatr_pos = (calc_v - (_vitems * 400)) / 2;
 
-    $('.js-background-grid').html('');
 
     for (var i = 0; i <= _hitems; i++) {
         var _hdelay = getRandom(0, 4000);
@@ -117,36 +145,13 @@ $(document).ready(function () {
 
     var _WIDTH = document.documentElement.scrollWidth;
     var _HEIGHT = document.documentElement.scrollHeight;
-    var _PIXEL = {
-        posX: 600,
-        posY: _HEIGHT / 2,
-        minposY: 0,
-        maxposY: _HEIGHT - 3,
-        speed: 10,
-        width: 3,
-        height: 3,
-        color: '#cccccc'
-    };
-    var _PREVEVENT, _CURREVENT;
-    $(document).mousemove(function (event) {
-        _CURREVENT = event;
-        if (_PREVEVENT && _CURREVENT) {
-            var movementX = Math.abs(_CURREVENT.screenX - _PREVEVENT.screenX);
-            var movementY = Math.abs(_CURREVENT.screenY - _PREVEVENT.screenY);
-            var speed = Math.sqrt(movementX * movementX + movementY * movementY);
-            _PIXEL.speed = speed;
-        }
-        _PREVEVENT = _CURREVENT;
-    });
 
     backgroundGridInit(_WIDTH, _HEIGHT);
-    backgroundCanvasInit(_PIXEL, _WIDTH, _HEIGHT);
+    backgroundCanvasInit(_WIDTH, _HEIGHT);
 
     $(window).on('resize', function () {
-        _WIDTH = document.documentElement.scrollWidth;
-        _HEIGHT = document.documentElement.scrollHeight;
         backgroundGridInit(_WIDTH, _HEIGHT);
-        backgroundCanvasInit(_PIXEL, _WIDTH, _HEIGHT);
+        backgroundCanvasInit(_WIDTH, _HEIGHT);
     });
 
 });
