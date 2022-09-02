f6c879b5f5305bfdbb8a1dff9e4b7ba6abf85412
Shepel Pavel <pavel@shepel.net>
2022-09-02 10:43:04 +0300

    upd: ci

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -4,7 +4,7 @@
     class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
     <link
-      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/docs/tailwind.min.css"
       rel="stylesheet"
     />
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  generate: {
+    dir: 'docs'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - resume',
